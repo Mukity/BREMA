306928b5edda1e10af2498180bd797e7383cb2e6
jacob <jacob97mukity@gmail.com>
2020-03-15 13:39:36 +0300

    Second

---
+++ BREMA/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+
+from . import views
+import BREMA
+
+urlpatterns = [
+    path('', views.HomePageView),
+    path('login', views.LoginView),
+    path('Login', views.Logout),
+
+    path('signup', views.SignupView),
+    path('Signup', views.Success),
+
+    path('Feedback_successful', views.Feedback),
+
+    path('donor', views.Donor),
+
+    path('hospital', views.Hospital),
+    path('hospital/', include('hospital.urls')),
+
+    path('requests', views.Requests),
+]
